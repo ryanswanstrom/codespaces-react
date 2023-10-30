fbb92b3e44657d3b64bdfad70501a7c68dde36e7
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2023-10-30 04:09:50 +0000

    adding an input form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('Hello');
+  const messageText = useRef();
+
+  function NewsLinks(e) {
+    e.preventDefault();
+    setMessage(messageText.current.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="logo512.png" className="App-logo" alt="logo" />
+        <img src="logo192.png" className="App-logo" alt="logo" />
         <p>
-          A simple React site built 
+          A simple React site built
           by <a className="App-link" href="https://ryanswanstrom.com">Ryan Swanstrom</a>
         </p>
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
       </header>
+      <article>
+        <form onSubmit={NewsLinks}>
+          <input type='text' ref={messageText} placeholder='What news do you want to find?'/>
+          <input type='submit' value='Click it!' />
+        </form>
+        <div>
+          <p>
+            {message}
+          </p>
+        </div>
+        <ol>
+          <li>Link 1</li>
+          <li>Link 2</li>
+        </ol>
+      </article>
     </div>
   );
 }
