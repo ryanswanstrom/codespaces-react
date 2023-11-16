e81a35f4a1628d77f9a2adc834a9125388ea6607
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2023-11-16 22:41:31 +0000

    get the page to appear

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 const newsFile = "https://newsdatafiles.blob.core.windows.net/news/news.json";
-
 
 function App() {
   const hostingApp = (process.env.REACT_APP_HOSTING_APP === undefined)?"Unknown":process.env.REACT_APP_HOSTING_APP;
@@ -20,38 +19,26 @@ function App() {
 
   return (
     <div className="App">
-      
       <header className="App-header">
         <h2>A simple React.js site built
           by <a className="App-link" href="https://ryanswanstrom.com">Ryan Swanstrom </a> 
           <br/>hosted on {hostingApp}
           </h2>
       </header>
-      <div className="user-container">
         <h3 className="info-item">Recent News for '{userData.query}'</h3> 
-        {userData.articles.map(art => (
-          <article> 
-            <h4>{art?.title} </h4>
-            <p>{art?.notes} <a href={art?.url}>...read more</a></p> 
-          </article>))}
-      </div>
       
-      <article>
-        <p>
-          Hosted on {hostingApp}
-        </p>
+      <article>        
         <p>
           See this same app on: 
-          <a href="https://news-vercel.ryanswanstrom.com"> Vercel</a> | 
-          <a href="https://news-render.ryanswanstrom.com"> Render</a> |
-          <a href="https://news-kinsta.ryanswanstrom.com"> Kinsta</a> |              
-          <a href="https://news-azure.ryanswanstrom.com"> Azure</a> |              
-          <a href="https://news-netlify.ryanswanstrom.com"> Netlify</a> |              
-          <a href="https://news-digitalocean.ryanswanstrom.com"> Digital Ocean</a> |              
-          <a href="https://news-cloudflare.ryanswanstrom.com"> Cloudflare Pages</a> 
+            <a href="https://news-vercel.ryanswanstrom.com"> Vercel</a> | 
+            <a href="https://news-render.ryanswanstrom.com"> Render</a> |
+            <a href="https://news-kinsta.ryanswanstrom.com"> Kinsta</a> |              
+            <a href="https://news-azure.ryanswanstrom.com"> Azure</a> |              
+            <a href="https://news-netlify.ryanswanstrom.com"> Netlify</a> |              
+            <a href="https://news-digitalocean.ryanswanstrom.com"> Digital Ocean</a> |              
+            <a href="https://news-cloudflare.ryanswanstrom.com"> Cloudflare Pages</a> 
         </p>
       </article>
-        
     </div>
   );
 }
